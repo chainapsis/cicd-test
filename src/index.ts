d6d6fd659685d1e3e14887a3660dd3b26310d362
HeesungB <hezo25@naver.com>
2022-10-20 18:36:43 +0900

    add: cron job

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-import { getChainIndetifier } from "./validation";
+// import { validateChainInfo } from "./validation";
+import { readFileSync } from "fs";
+import { ChainInfo } from "@keplr-wallet/types";
+import { ChainUpdaterService } from "@keplr-wallet/background";
+
+const getFileToJson = (filePath: string) => {
+  const file = readFileSync(filePath, "utf-8");
+  const chainInfo: ChainInfo = JSON.parse(file);
+  return chainInfo;
+};
 
 const main = async () => {
+  // get file name
   const args = process.argv.slice(2);
-  console.log("args", args);
 
-  const test = getChainIndetifier("cosmoshub-1");
+  // get json from file
+  const chainInfo = getFileToJson(args[0]);
+
+  // validate chain information
+  const test = await ChainUpdaterService.checkChainUpdate(chainInfo);
   console.log(test);
 };
 
