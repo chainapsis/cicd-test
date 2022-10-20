f4dd5ce4caadcf19e13deb6fb122bbb846440c29
HeesungB <hezo25@naver.com>
2022-10-20 17:36:07 +0900

    add: get chanaged files library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { getChainIndetifier } from "./validation";
 
 const main = async () => {
+  const args = process.argv.slice(2);
+  console.log("args", args);
+
   const test = getChainIndetifier("cosmoshub-1");
   console.log(test);
 };
