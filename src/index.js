7f4b1645ef023828f5d494bf090d6edad87194a6
HeesungB <hezo25@naver.com>
2022-10-20 20:42:56 +0900

    fix: cronjob fetch time

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const validator = require("./validator.js");
-
-test();
-
-async function test() {
-  const checkValidateResult = await validator.checkValidate();
-  console.log("checkValidateResult = " + JSON.stringify(checkValidateResult));
-}
